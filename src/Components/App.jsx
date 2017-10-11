bc74a55be4f9e8e4f075bc2c48650d4041842533
shreyas-a <shreyas.a24@gmail.com>
2017-10-11 11:07:51 +0530

    added airbnb eslint

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,19 +6,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      developers: [],
-      search: ""
+      developers: []
     };
+
+    this.searchDeveloper = this.searchDeveloper.bind(this);
   }
 
   componentDidMount() {
-    let userName = "shreyas-a";
-    if (location.host && location.host.indexOf(".github.io") !== -1) {
-      userName = location.host.split(".github.io")[0];
-    }
-
     fetch(
-      `https://api.github.com/repos/${userName}/awesome-developers/contents/developers.json`
+      `https://api.github.com/repos/shreyas-a/awesome-developers/contents/developers.json`
     )
       .then(response => response.json())
       .then(jsonResponse => atob(jsonResponse.content))
@@ -33,27 +29,22 @@ class App extends Component {
 
   searchDeveloper(event) {
     let updatedList = developers;
-    updatedList = updatedList.filter(item => {
-      // TODO: A better comparison condition
-      return (
+    updatedList = updatedList.filter(
+      item =>
+        // TODO: A better comparison condition
         item.name.toLowerCase().search(event.target.value.toLowerCase()) !==
           -1 ||
         item.company.toLowerCase().search(event.target.value.toLowerCase()) !==
           -1 ||
-        item.github.toLowerCase().search(event.target.value.toLowerCase()) !==
-          -1 ||
         item.city.toLowerCase().search(event.target.value.toLowerCase()) !==
           -1 ||
+        item.github.toLowerCase().search(event.target.value.toLowerCase()) !==
+          -1 ||
+        item.email.toLowerCase().search(event.target.value.toLowerCase()) !==
+          -1 ||
         item.skills.find(s => s.includes(event.target.value.toLowerCase()))
-      );
-    });
+    );
     this.setState({ developers: updatedList });
-  }
-
-  componentWillMount() {
-    this.setState({
-      developers
-    });
   }
 
   render() {
@@ -63,7 +54,7 @@ class App extends Component {
         <input
           type="text"
           placeholder="Search"
-          onChange={this.searchDeveloper.bind(this)}
+          onChange={this.searchDeveloper}
         />
         <div>
           <p>A curated list of awesome developers.</p>
@@ -73,13 +64,14 @@ class App extends Component {
                 <img
                   src={`https://avatars.githubusercontent.com/${developer.github}`}
                   className="profile-image"
-                  alt="Profile Image"
+                  alt="profile"
                 />
                 <div>{developer.name}</div>
                 <div>{developer.skills.join(", ")}</div>
                 <div>{developer.company}</div>
                 <div>{developer.city}</div>
                 <div>{developer.github}</div>
+                <div>{developer.email}</div>
               </div>
             ))}
           </div>
