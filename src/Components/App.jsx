76350464c8912cd076b75c3d58041eca483302bf
ashwinkshenoy <ashwinkshenoy@gmail.com>
2017-10-11 16:46:11 +0530

    Basic styling

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -33,7 +33,7 @@ class App extends Component {
       item =>
         // TODO: A better comparison condition
         item.name.toLowerCase().search(event.target.value.toLowerCase()) !==
-          -1 ||
+        -1 ||
         item.company.toLowerCase().search(event.target.value.toLowerCase()) !==
           -1 ||
         item.city.toLowerCase().search(event.target.value.toLowerCase()) !==
@@ -50,32 +50,56 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Awesome Developers</h1>
-        <input
-          type="text"
-          placeholder="Search"
-          onChange={this.searchDeveloper}
-        />
+        <br />
+        <div className="hd1">
+          Awesome Developers
+          <br />
+          <small>Curated list of awesome dev's Around the World</small>
+        </div>
+
+        <div className="search-div">
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={this.searchDeveloper.bind(this)}
+            className="search"
+          />
+        </div>
+
         <div>
-          <p>A curated list of awesome developers.</p>
           <div className="developer-list">
             {this.state.developers.map((developer, index) => (
               <div className="developer" key={index}>
-                <img
-                  src={`https://avatars.githubusercontent.com/${developer.github}`}
-                  className="profile-image"
-                  alt="profile"
-                />
-                <div>{developer.name}</div>
-                <div>{developer.skills.join(", ")}</div>
-                <div>{developer.company}</div>
-                <div>{developer.city}</div>
-                <div>{developer.github}</div>
-                <div>{developer.email}</div>
+                <div className="dev">
+                  <div className="dev-avatar">
+                    <img
+                      src={`https://avatars.githubusercontent.com/${developer.github}`}
+                      className="profile-image"
+                      alt="Profile Image"
+                    />
+                  </div>
+                  <div className="dev-desc">
+                    <div className="dev--name">{developer.name}</div>
+                    <div className="dev--company">{developer.company}</div>
+                    <div className="dev--city">{developer.city}</div>
+                    <div className="dev--email">{developer.email}</div>
+                  </div>
+                  <div className="dev--social">
+                    <a href={'https://github.com/'+developer.github} target="_blank">
+                      <img src="/img/git.svg" alt="git"/>
+                    </a>                      
+                  </div>
+                </div>
+                <div className="dev--skills">
+                  {developer.skills.map((skill, index) => (
+                    <span key={index}>{skill}</span>
+                  ))}
+                </div>
               </div>
             ))}
           </div>
         </div>
+
       </div>
     );
   }
